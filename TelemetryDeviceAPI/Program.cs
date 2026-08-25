0cc71966d8ed51de184de85968c25b43e29279fc
idanSA7 <idansalhani@gmail.com>
2026-08-25 15:54:55 +0300

    [REFACTOR] remove unnecessary comments and lambda in program.cs

--- TelemetryDeviceAPI/Program.cs
+++ TelemetryDeviceAPI/Program.cs
@@ -1,37 +1,21 @@
-using TelemetryDeviceAPI.Services;
 using DecoderLibrary;
 using IcdModelsLIbrary;
+using TelemetryDeviceAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Kafka Producer Service registration
 builder.Services.AddSingleton<IKafkaProducerService, KafkaProducerService>();
-
-// Decoder Flow registration
 builder.Services.AddSingleton<DecoderFlow>();
-
-// ICD Model registration
-builder.Services.AddSingleton<IcdModel>(sp =>
-{
-    // Replace with your actual ICD loading logic if loaded from JSON or file
-    return new IcdModel();
-});
-
-// Packet Queue Pipeline Service registration
+builder.Services.AddSingleton<IcdModel>();
 builder.Services.AddSingleton<IPacketQueueService, PacketQueueService>();
-
-// Sniffer Service registration
 builder.Services.AddSingleton<ISnifferService, SnifferService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -39,9 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
