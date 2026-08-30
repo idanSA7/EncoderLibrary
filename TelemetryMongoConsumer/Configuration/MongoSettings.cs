d620b049df9d36c4f93346cea34bd9f0da06b11f
idanSA7 <idansalhani@gmail.com>
2026-08-30 10:26:58 +0300

    [SETUP] Initialize Mongo consumer project and database configurations

---
+++ TelemetryMongoConsumer/Configuration/MongoSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TelemetryMongoConsumer.Configuration
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DBName { get; set; } = string.Empty;
+        public string CollectionName { get; set; } = string.Empty;
+    }
+}
