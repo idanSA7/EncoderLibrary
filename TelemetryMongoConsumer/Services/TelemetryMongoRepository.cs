5e374411c93d2af548c4d667d3dd3c1b9e0964be
idanSA7 <idansalhani@gmail.com>
2026-09-01 12:56:04 +0300

    [FEAT] Implement Kafka consumer worker and MongoDB persistence repository

---
+++ TelemetryMongoConsumer/Services/TelemetryMongoRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using TelemetryMongoConsumer.Configuration;
+using TelemetryMongoConsumer.Models;
+using TelemetryMongoConsumer.Interfaces;
+namespace TelemetryMongoConsumer.Services
+{
+    
+
+    public class TelemetryMongoRepository : ITelemetryMongoRepository
+    {
+        private readonly IMongoCollection<DecodedPacketDocument> _collection;
+
+        public TelemetryMongoRepository(
+            IMongoClient mongoClient,
+            IOptions<MongoSettings> mongoOptions)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(mongoOptions.Value.DBName);
+            _collection = database.GetCollection<DecodedPacketDocument>(mongoOptions.Value.CollectionName);
+        }
+
+        public async Task InsertDecodedPacketAsync(DecodedPacketDocument document)
+        {
+            await _collection.InsertOneAsync(document);
+        }
+    }
+}
